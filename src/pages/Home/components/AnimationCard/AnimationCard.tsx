8a0aec15791365ae6c4accb7e5dd987401d5093a
김경모 <gm.kim333@gmail.com>
2024-06-26 21:56:42 +0900

    feat 홈 화면에서 좋아요한 애니 표시

--- src/pages/Home/components/AnimationCard/AnimationCard.tsx
+++ src/pages/Home/components/AnimationCard/AnimationCard.tsx
@@ -1,7 +1,7 @@
 import styles from './AnimationCard.module.scss';
 import { useNavigate } from 'react-router-dom';
 interface Props {
-    data: animation;
+    data: AnimationResponse;
 }
 function AnimationCard({ data }: Props) {
     const navigate = useNavigate();
@@ -11,7 +11,12 @@ function AnimationCard({ data }: Props) {
     return (
         <div className={styles.container} onClick={handleClickCard}>
             <div className={styles.thumbnail__container}>
-                <img className={styles.thumbnail} src={data.thumbnailUrl} alt={data.name + '_썸네일'} />
+                <div className={`${styles.favorite} ${data.favorite.isFavorite && styles.liked}`}>
+                    <span className="material-symbols-outlined">favorite</span>
+                </div>
+                <div className={styles.thumbnail__container}>
+                    <img className={styles.thumbnail} src={data.thumbnailUrl} alt={data.name + '_썸네일'} />
+                </div>
             </div>
             <div className={styles.info}>
                 <div className={styles.title}>{data.name}</div>
